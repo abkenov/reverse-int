51a70eea1ddffd2f37858c5d80c1c67a792f0f0a
Ablan Abkenov <abkenov.ablan@gmail.com>
2020-09-30 07:12:47 +0600

    init: implement reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let temp = n.toString()
+    let result = ""
+    
+    for (digit of temp) {
+        result = digit + result
+    }
+
+    return parseInt(result)
 }
